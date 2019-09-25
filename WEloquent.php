853aa8ff322cd023b96b6e0597f0d51eb6e2069f
Alexis Coulombe <alexis.410@hotmail.com>
2019-09-25 17:34:11 -0400

    Added comment + more functions

--- WEloquent.php
+++ WEloquent.php
@@ -2,28 +2,36 @@
     
     class WEloquent
     {
+        /** @var array $filters */
         private $filters = [];
+        
+        private function check_arguments($param, array $expected_params) {
+            if(!in_array(gettype($param), $expected_params)){
+                throw new InvalidArgumentException('Expected arguments are ' . $expected_params);
+            }
+        }
         
         /**
          * Run WP_Query
-         * @return WP_Query
+         * @return array
          */
         public function execute(): array {
+            /** @var WP_Query $query */
             $query = new WP_Query($this->filters);
             
             if($query->have_posts()){
                 return $query->posts;
             }
-            
+
             return [];
         }
     
         /**
          * Show posts based on a keyword search.
          * @param string $search
-         * @return WEloquent
+         * @return $this
          */
-        public function search(string $search): WEloquent {
+        public function search(string $search) {
             $this->filters['s'] = $search;
     
             return $this;
@@ -32,9 +40,9 @@
         /**
          * Get maximum of posts per page
          * @param int $per_page
-         * @return WEloquent
+         * @return $this
          */
-        public function posts_per_page(int $per_page): WEloquent {
+        public function posts_per_page(int $per_page) {
             $this->filters['posts_per_page'] = $per_page;
             
             return $this;
@@ -42,32 +50,88 @@
         
         /**
          * Set post type for search
-         * @param string $post_type
-         * @return WEloquent|null
+         * @param $post_type
+         * @return $this
          */
-        public function post_type(string $post_type = 'posts'): WEloquent {
+        public function post_type($post_type = 'posts') {
+            $this->check_arguments($post_type, ['string', 'array']);
+            
             $this->filters['post_type'] = $post_type;
             
             return $this;
         }
     
         /**
-         * Get posts by post slug
-         * @param string $slug
-         * @return WEloquent
+         * Ignore sticky posts to prevent prepend
+         * @param bool $ignore
+         * @return $this
          */
-        public function post_slug(string $slug): WEloquent{
-            $this->filters['name'] = $slug;
+        public function ignore_sticky_posts(bool $ignore) {
+            $this->filters['ignore_sticky_posts'] = $ignore;
     
+            return $this;
+        }
+    
+        /**
+         * Offset posts results
+         * @param int $offset
+         * @return $this
+         */
+        public function offset(int $offset) {
+            $this->filters['offset'] = $offset;
+    
+            return $this;
+        }
+    
+        /**
+         * Get posts by post name
+         * @param string $name
+         * @return $this
+         */
+        public function post_name(string $name){
+            $this->filters['name'] = $name;
+    
+            return $this;
+        }
+    
+        /**
+         * Get posts by post slug
+         * @param string $name
+         * @return $this
+         */
+        public function post_slug(string $slug){
+            $this->filters['pagename'] = $slug;
+        
+            return $this;
+        }
+    
+        public function comment_count($count) {
+            $this->check_arguments($count, ['int', 'array']);
+            
+            $this->filters['comment_count'] = $count;
+            
+            return $this;
+        }
+        
+        /**
+         * Get posts by status
+         * @param string|array $status
+         * @return $this
+         */
+        public function post_status($status) {
+            $this->check_arguments($status, ['string', 'array']);
+            
+            $this->filters['post_status'] = $status;
+            
             return $this;
         }
     
         /**
          * Use page id to return only child pages. Set to 0 to return only top-level entries.
          * @param int $parent_id
-         * @return WEloquent
+         * @return $this
          */
-        public function post_parent(int $parent_id): WEloquent{
+        public function post_parent(int $parent_id){
             $this->filters['post_parent'] = $parent_id;
         
             return $this;
@@ -76,9 +140,9 @@
         /**
          * Specify posts whose parent is in an array
          * @param array $parent_ids
-         * @return WEloquent
+         * @return $this
          */
-        public function post_parent__in(array $parent_ids): WEloquent{
+        public function post_parent__in(array $parent_ids){
             $this->filters['post_parent__in'] = $parent_ids;
         
             return $this;
@@ -87,9 +151,9 @@
         /**
          * Use post ids. Specify posts whose parent is not in an array.
          * @param array $parent_ids
-         * @return WEloquent
+         * @return $this
          */
-        public function post_parent__not_in(array $parent_ids): WEloquent{
+        public function post_parent__not_in(array $parent_ids){
             $this->filters['post_parent__not_in'] = $parent_ids;
     
             return $this;
@@ -98,9 +162,9 @@
         /**
          * Specify posts to retrieve
          * @param array $ids
-         * @return WEloquent
+         * @return $this
          */
-        public function post__in(array $ids): WEloquent{
+        public function post__in(array $ids){
             $this->filters['post__in'] = $ids;
     
             return $this;
@@ -109,9 +173,9 @@
         /**
          * Specify post NOT to retrieve
          * @param array $ids
-         * @return WEloquent
+         * @return $this
          */
-        public function post__not_in(array $ids): WEloquent{
+        public function post__not_in(array $ids){
             $this->filters['post___not_in'] = $ids;
         
             return $this;
@@ -120,9 +184,9 @@
         /**
          * Use post slugs. Specify posts to retrieve
          * @param array $slugs
-         * @return WEloquent
+         * @return $this
          */
-        public function post_name__in(array $slugs): WEloquent{
+        public function post_name__in(array $slugs){
             $this->filters['post_name__in'] = $slugs;
         
             return $this;
@@ -133,7 +197,7 @@
          * @param array $params
          * @return mixed
          */
-        public function author(array $params): WEloquent {
+        public function author(array $params) {
             array_merge($params, $this->filters);
             
             return $this;
@@ -142,9 +206,9 @@
         /**
          * Get posts by author id
          * @param int $id
-         * @return WEloquent
+         * @return $this
          */
-        public function author_id(int $id): WEloquent {
+        public function author_id(int $id) {
             $this->filters['author'] = $id;
             
             return $this;
@@ -153,9 +217,9 @@
         /**
          * Get posts by author name
          * @param string $name
-         * @return WEloquent
+         * @return $this
          */
-        public function author_name(string $name): WEloquent {
+        public function author_name(string $name) {
             $this->filters['author_name'] = $name;
             
             return $this;
@@ -164,9 +228,9 @@
         /**
          * Get posts by author in array
          * @param array $in
-         * @return WEloquent
+         * @return $this
          */
-        public function author__in(array $in): WEloquent {
+        public function author__in(array $in) {
             $this->filters['author__in'] = $in;
             
             return $this;
@@ -175,9 +239,9 @@
         /**
          * Get posts by author not in array
          * @param array $not_in
-         * @return WEloquent
+         * @return $this
          */
-        public function author__not_in(array $not_in): WEloquent {
+        public function author__not_in(array $not_in) {
             $this->filters['author__not_in'] = $not_in;
             
             return $this;
@@ -188,7 +252,7 @@
          * @param array $params
          * @return mixed
          */
-        public function category(array $params): WEloquent {
+        public function category(array $params) {
             array_merge($params, $this->filters);
             
             return $this;
@@ -197,9 +261,9 @@
         /**
          * Get posts by category id
          * @param int $id
-         * @return WEloquent
+         * @return $this
          */
-        public function category_id(int $id): WEloquent {
+        public function category_id(int $id) {
             $this->filters['cat'] = $id;
             
             return $this;
@@ -208,9 +272,9 @@
         /**
          * Get posts by category name
          * @param string $name
-         * @return WEloquent
+         * @return $this
          */
-        public function category_name(string $name): WEloquent {
+        public function category_name(string $name) {
             $this->filters['category_name'] = $name;
             
             return $this;
@@ -219,9 +283,9 @@
         /**
          * Get posts in multiple categories
          * @param array $ids
-         * @return WEloquent
+         * @return $this
          */
-        public function category__and(array $ids): WEloquent {
+        public function category__and(array $ids) {
             $this->filters['category__and'] = $ids;
             
             return $this;
@@ -230,9 +294,9 @@
         /**
          * Get posts by category in ids
          * @param array $ids
-         * @return WEloquent
+         * @return $this
          */
-        public function category__in(array $ids): WEloquent {
+        public function category__in(array $ids) {
             $this->filters['category__in'] = $ids;
             
             return $this;
@@ -241,9 +305,9 @@
         /**
          * Get posts by category not in ids
          * @param array $ids
-         * @return WEloquent
+         * @return $this
          */
-        public function category__not_in(array $ids): WEloquent {
+        public function category__not_in(array $ids) {
             $this->filters['category__not_in'] = $ids;
             
             return $this;
@@ -254,7 +318,7 @@
          * @param array $params
          * @return mixed
          */
-        public function tag(array $params): WEloquent {
+        public function tag(array $params) {
             array_merge($params, $this->filters);
             
             return $this;
@@ -263,9 +327,9 @@
         /**
          * Get posts by tag id
          * @param int $id
-         * @return WEloquent
+         * @return $this
          */
-        public function tag_id(int $id): WEloquent {
+        public function tag_id(int $id) {
             $this->filters['tag_id'] = $id;
             
             return $this;
@@ -274,9 +338,9 @@
         /**
          * Get posts with multiple tags
          * @param array $ids
-         * @return WEloquent
+         * @return $this
          */
-        public function tag__and(array $ids): WEloquent {
+        public function tag__and(array $ids) {
             $this->filters['tag__and'] = $ids;
             
             return $this;
@@ -285,9 +349,9 @@
         /**
          * Get posts by tag in ids
          * @param array $ids
-         * @return WEloquent
+         * @return $this
          */
-        public function tag__in(array $ids): WEloquent {
+        public function tag__in(array $ids) {
             $this->filters['tag__in'] = $ids;
             
             return $this;
@@ -296,9 +360,9 @@
         /**
          * Get posts by tag not in ids
          * @param array $ids
-         * @return WEloquent
+         * @return $this
          */
-        public function tag__not_in(array $ids): WEloquent {
+        public function tag__not_in(array $ids) {
             $this->filters['tag__not_in'] = $ids;
             
             return $this;
@@ -307,9 +371,9 @@
         /**
          * Get posts with multiple tag slugs
          * @param array $slugs
-         * @return WEloquent
+         * @return $this
          */
-        public function tag_slug__and(array $slugs): WEloquent {
+        public function tag_slug__and(array $slugs) {
             $this->filters['tag_slug__and'] = $slugs;
             
             return $this;
@@ -318,9 +382,9 @@
         /**
          * Get posts by tag slug in slugs
          * @param array $slugs
-         * @return WEloquent
+         * @return $this
          */
-        public function tag_slug__in(array $slugs): WEloquent {
+        public function tag_slug__in(array $slugs) {
             $this->filters['tag_slug__in'] = $slugs;
             
             return $this;
@@ -331,7 +395,7 @@
          * @param array $params
          * @return mixed
          */
-        public function taxonomy(array $params): WEloquent {
+        public function taxonomy(array $params) {
             array_merge($params, $this->filters);
             
             return $this;
@@ -340,9 +404,9 @@
         /**
          * The logical relationship between each inner taxonomy array when there is more than one.
          * @param string $relation
-         * @return WEloquent
+         * @return $this
          */
-        public function tax_relation(string $relation = 'AND'): WEloquent {
+        public function tax_relation(string $relation = 'AND') {
             if (!isset($this->filters['tax_query'])) {
                 $this->filters['tax_query'] = [];
             }
@@ -355,9 +419,9 @@
         /**
          * Set taxonomy
          * @param string $taxonomy
-         * @return WEloquent
+         * @return $this
          */
-        public function tax_name(string $taxonomy): WEloquent {
+        public function tax_name(string $taxonomy) {
             if (!isset($this->filters['tax_query'])) {
                 $this->filters['tax_query'] = [];
             }
@@ -371,9 +435,9 @@
          * Set taxonomy search field
          * @param string $field
          * @param int    $index
-         * @return WEloquent
+         * @return $this
          */
-        public function tax_field(string $field = 'term_id', int $index = 0): WEloquent {
+        public function tax_field(string $field = 'term_id', int $index = 0) {
             if (!isset($this->filters['tax_query'])) {
                 $this->filters['tax_query'] = [];
             }
@@ -387,9 +451,9 @@
          * Set taxonomy search terms
          * @param array $terms
          * @param int   $index
-         * @return WEloquent
+         * @return $this
          */
-        public function tax_terms(array $terms, int $index = 0): WEloquent {
+        public function tax_terms(array $terms, int $index = 0) {
             if (!isset($this->filters['tax_query'])) {
                 $this->filters['tax_query'] = [];
             }
@@ -403,9 +467,9 @@
          * Whether or not to include children for hierarchical taxonomies.
          * @param bool $include
          * @param int  $index
-         * @return WEloquent
+         * @return $this
          */
-        public function tax_include_children(bool $include = true, int $index = 0): WEloquent {
+        public function tax_include_children(bool $include = true, int $index = 0) {
             if (!isset($this->filters['tax_query'])) {
                 $this->filters['tax_query'] = [];
             }
@@ -419,9 +483,9 @@
          * Operator to test
          * @param string $operator
          * @param int    $index
-         * @return WEloquent
+         * @return $this
          */
-        public function tax_operator(string $operator = 'IN', int $index = 0): WEloquent {
+        public function tax_operator(string $operator = 'IN', int $index = 0) {
             if (!isset($this->filters['tax_query'])) {
                 $this->filters['tax_query'] = [];
             }
@@ -434,9 +498,9 @@
         /**
          * Posts order by
          * @param array $order_by
-         * @return WEloquent|null
+         * @return $this
          */
-        public function order_by(array $order_by = ['title' => 'ASC']): WEloquent {
+        public function order_by(array $order_by = ['title' => 'ASC']) {
             if (!isset($this->filters['orderby'])) {
                 $this->filters['orderby'] = [];
             }
@@ -449,9 +513,9 @@
         /**
          * Posts order
          * @param string $order
-         * @return WEloquent|null
+         * @return $this
          */
-        public function order(string $order = 'ASC'): WEloquent {
+        public function order(string $order = 'ASC') {
             $this->filters['order'] = $order;
             
             return $this;
